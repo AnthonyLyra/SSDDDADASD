80b9946f47fcec8ed3934b094d101a7bc84c92bc
/Spexxz <154355710+AnthonyLyra@users.noreply.github.com>
2025-12-04 09:54:58 -0300

    Refactor table display logic and add average calculation
    Refactor functions to use display property for tables and error paragraph. Added calcularMedia function to calculate and display average scores.

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
-const valueInput = document.getElementById('InputNomePrin')
-const ATable = document.getElementById('tableAnthony')
-const GTable = document.getElementById('tableGabriel')
-const YTable = document.getElementById('tableYuri')
-const errParagraph = document.getElementById('paragraph')
-ATable.setAttribute('hidden', true)
-GTable.setAttribute('hidden', true)
-YTable.setAttribute('hidden', true)
-errParagraph.setAttribute('hidden', true)
+const valueInput = document.getElementById('InputNomePrin');
+const ATable = document.getElementById('tableAnthony');
+const GTable = document.getElementById('tableGabriel');
+const YTable = document.getElementById('tableYuri');
+const errParagraph = document.getElementById('paragraph');
+
+function esconderTudo() {
+    ATable.style.display = 'none';
+    GTable.style.display = 'none';
+    YTable.style.display = 'none';
+    errParagraph.style.display = 'none';
+}
+esconderTudo()
+
+function calcularMedia() {
+
+    esconderTudo()
+    let linha = document.querySelectorAll("tbody tr")
+
+    linha.forEach(linha => {
+
+        let n1 = parseFloat(linha.cells[1].innerText.replace(",", "."));
+        let n2 = parseFloat(linha.cells[2].innerText.replace(",", "."));
+        let n3 = parseFloat(linha.cells[3].innerText.replace(",", "."));
+
+        let resultMedia = (n1 + n2 + n3) / 3;
+
+        linha.cells[5].innerHTML = resultMedia.toFixed(1).replace(".", ",");
+    });
+}
+
+calcularMedia(ATable)
+calcularMedia(GTable)
+calcularMedia(YTable)
 
 function NomeFunc() {
-    const valueInputMi = valueInput.value
+    const valueInputMi = valueInput.value;
+    const pA = document.getElementById('p_Anthony')
+    const pG = document.getElementById('p_Gabriel')
+    const pY = document.getElementById('p_Yuri')
+
+    esconderTudo()
+
     if (valueInputMi === 'Anthony') {
-    YTable.setAttribute('hidden', true)
-    GTable.setAttribute('hidden', true)
-    ATable.removeAttribute('hidden')
-    errParagraph.setAttribute('hidden', true)
-    }
-    else if (valueInputMi === 'Gabriel') {
-    ATable.setAttribute('hidden', true)
-    YTable.setAttribute('hidden', true)
-    GTable.removeAttribute('hidden')
-    errParagraph.setAttribute('hidden', true)
-    }
-    else if (valueInputMi === 'Yuri') {
-    ATable.setAttribute('hidden', true)
-    GTable.setAttribute('hidden', true)
-    YTable.removeAttribute('hidden')
-    errParagraph.setAttribute('hidden', true)
-    }
-    else if (valueInput !== 'Anthony', 'Yuri', 'Gabriel') {
-        errParagraph.removeAttribute('hidden')
-        ATable.setAttribute('hidden', true)
-        GTable.setAttribute('hidden', true)
-        YTable.setAttribute('hidden', true)
+        ATable.style.display = 'table';
+        pA.innerHTML = 'Estudante: Anthony'
+        pA.style.display = 'table'
+        pY.style.display = 'none'
+        pG.style.display = 'none'
+    } else if (valueInputMi === 'Gabriel') {
+        GTable.style.display = 'table';
+        pG.innerHTML = ('Estudante: Gabriel')
+        pG.style.display = 'table'
+        pA.style.display = 'none'
+        pY.style.display = 'none'
+    } else if (valueInputMi === 'Yuri') {
+        YTable.style.display = 'table';
+        pY.innerHTML = ('Estudante: Yuri')
+        pY.style.display = 'table'
+        pA.style.display = 'none'
+        pG.style.display = 'none'
+    } else {
+        errParagraph.style.display = 'block';
     }
 }
